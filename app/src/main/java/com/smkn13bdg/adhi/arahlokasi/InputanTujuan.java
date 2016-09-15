be80cd1556b5c760fcfb2728707f86f311b94512
adhimeong <adhimeong@gmail.com>
2016-09-15 13:02:13 +0700

    nambah halaman inputan tujuan

---
+++ app/src/main/java/com/smkn13bdg/adhi/arahlokasi/InputanTujuan.java
@@ -0,0 +1,18 @@
+package com.smkn13bdg.adhi.arahlokasi;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by adhi on 15/09/16.
+ */
+public class InputanTujuan extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.input_tujuan);
+    }
+
+}
