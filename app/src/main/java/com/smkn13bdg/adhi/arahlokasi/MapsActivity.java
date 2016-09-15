b407a12a32619d8bee5d6e567523c2911e0c1975
adhimeong <adhimeong@gmail.com>
2016-09-15 13:36:42 +0700

    nambah halaman inputan tujuan

--- app/src/main/java/com/smkn13bdg/adhi/arahlokasi/MapsActivity.java
+++ app/src/main/java/com/smkn13bdg/adhi/arahlokasi/MapsActivity.java
@@ -29,8 +29,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng smkn13 = new LatLng(-6.938195, 107.656938);
+        mMap.addMarker(new MarkerOptions().position(smkn13).title("SMKN 13 Bandung"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(smkn13));
     }
 }
