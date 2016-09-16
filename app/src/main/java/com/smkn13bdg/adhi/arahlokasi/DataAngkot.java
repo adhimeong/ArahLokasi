6a3dbad37025434c2e251d119c57f561ded9aa4f
adhimeong <adhimeong@gmail.com>
2016-09-16 08:17:40 +0700

    penambahan direction menggunakan library jd-alexander

---
+++ app/src/main/java/com/smkn13bdg/adhi/arahlokasi/DataAngkot.java
@@ -0,0 +1,7 @@
+package com.smkn13bdg.adhi.arahlokasi;
+
+/**
+ * Created by adhi on 16/09/16.
+ */
+public class DataAngkot {
+}
--- app/src/main/java/com/smkn13bdg/adhi/arahlokasi/MapsActivity.java
+++ app/src/main/java/com/smkn13bdg/adhi/arahlokasi/MapsActivity.java
@@ -1,6 +1,5 @@
 package com.smkn13bdg.adhi.arahlokasi;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
