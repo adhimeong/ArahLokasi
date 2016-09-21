ed7dee2a26ccae1f08879f9b003310a95ccde598
adhimeong <adhimeong@gmail.com>
2016-09-21 08:59:22 +0700

    penambahan direction menggunakan library jd-alexander

--- app/src/main/java/com/smkn13bdg/adhi/arahlokasi/DataAngkot.java
+++ app/src/main/java/com/smkn13bdg/adhi/arahlokasi/DataAngkot.java
@@ -1,7 +1,34 @@
 package com.smkn13bdg.adhi.arahlokasi;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
 /**
  * Created by adhi on 19/09/16.
  */
-public class DataAngkot {
+public class DataAngkot extends AppCompatActivity {
+
+    public TextView asalview, tujuanview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setlayout
+        setContentView(R.layout.cobaintent);
+        //cari id view
+        asalview = (TextView)findViewById(R.id.textView3);
+        tujuanview = (TextView)findViewById(R.id.textView4);
+        //perintah
+        ambilData();
+    }
+
+    private void ambilData(){
+        Bundle data = getIntent().getExtras();
+
+        asalview.setText(data.getCharSequence("asalintent"));
+        tujuanview.setText(data.getCharSequence("tujuanintent"));
+    }
+
 }
--- app/src/main/java/com/smkn13bdg/adhi/arahlokasi/InputanTujuan.java
+++ app/src/main/java/com/smkn13bdg/adhi/arahlokasi/InputanTujuan.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
@@ -12,10 +13,14 @@ import android.widget.AutoCompleteTextView;
  */
 public class InputanTujuan extends AppCompatActivity{
 
+    public AutoCompleteTextView asaltext;
+    public AutoCompleteTextView tujuantext;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.input_tujuan);
+
 
         //autocomplete
         //array temapt asal
@@ -25,18 +30,31 @@ public class InputanTujuan extends AppCompatActivity{
         ArrayAdapter<String> tujuanadapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_dropdown_item_1line, TUJUAN);
         //cari id di layout
-        AutoCompleteTextView asaltext = (AutoCompleteTextView)
+        asaltext = (AutoCompleteTextView)
                 findViewById(R.id.autoCompleteAwal);
-        AutoCompleteTextView tujuantext = (AutoCompleteTextView)
+        tujuantext = (AutoCompleteTextView)
                 findViewById(R.id.autoCompleteTujuan);
         //masukan data array
+        asaltext.setThreshold(1);//mulai kerja pada inputan ke 1
+        tujuantext.setThreshold(1); //mulai kerja pada inputan ke 1
         asaltext.setAdapter(asaladapter);
         tujuantext.setAdapter(tujuanadapter);
+
     }
 
     public void cariArah (View view ) {
-        Intent intent = new Intent(this, MapsActivity.class);
-        startActivity(intent);
+        Intent halaman = new Intent(getApplicationContext(), DataAngkot.class);
+        //membuat objek bundle
+        Bundle data = new Bundle();
+
+        //menyisipkan string
+        data.putString("asalintent", tujuantext.getText().toString());
+        data.putString("tujuanintent", tujuantext.getText().toString());
+
+        //menambah bundle intent
+        halaman.putExtras(data);
+        //mulai activity
+        startActivity(halaman);
     }
 
     private static final String[] ASAL = new String[] {
