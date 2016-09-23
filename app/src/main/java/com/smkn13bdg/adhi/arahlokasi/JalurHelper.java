6b03cead8052e401b83b45f053dff093ae704903
adhimeong <adhimeong@gmail.com>
2016-09-23 13:20:26 +0700

    data angkot dimasukan..

---
+++ app/src/main/java/com/smkn13bdg/adhi/arahlokasi/JalurHelper.java
@@ -0,0 +1,13 @@
+package com.smkn13bdg.adhi.arahlokasi;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by adhi on 23/09/16.
+ */
+
+public class JalurHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME ="angkotbdg";
+
+}
